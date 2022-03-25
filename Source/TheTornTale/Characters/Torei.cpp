28fd6fca4a1fd81c13b78631ae31f5542e3e65a4
Asraf <noorworkz@gmail.com>
2022-03-25 16:23:23 +0530

    Added full path to Torei.cpp

--- Source/TheTornTale/Characters/Torei.cpp
+++ Source/TheTornTale/Characters/Torei.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Characters/Torei.h"
+#include "TheTornTale/Characters/Torei.h"
 
 // Sets default values
 ATorei::ATorei()
